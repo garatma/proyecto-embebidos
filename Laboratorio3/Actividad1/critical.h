63412a6dd97e6db68e7d0e6c9d78c67afb90be51
Manuel Garat <m.garat@outlook.com>
2019-11-13 10:22:43 -0300

    deleted unnecesary files

--- Laboratorio3/Actividad1/critical.h
+++
@@ -1,7 +0,0 @@
-#ifndef CRITICAL_C
-#define CRITICAL_C
-
-void critical_begin(void);
-void critical_end(void);
-
-#endif
--- Laboratorio3/Actividad2/aplicacion-host/processing-build.sh
+++
@@ -1,2 +0,0 @@
-rm -rf build
-processing-java --sketch="SE_Lab_HostTempApp" --output="build" --export 
